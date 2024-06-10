05721f85a2510e092ba631125511b2e095bf8e55
Christoph <chrisi.niederer@gmail.com>
2024-06-10 12:29:00 +0200

    Complete questions and add button

--- templates/Questions.js
+++ templates/Questions.js
@@ -4,44 +4,49 @@ const questions = [
     "Ich plane bei meiner Anreise immer einen zeitlichen Puffer ein.",
     "Ich habe im letzten Jahr nicht einen Termin verpasst.",
     "Wenn ich merke ich schaffe es nicht pünktlich zu sein, informiere ich meinen Gegenüber rechtzeitig.",
-    //Durchsetzungsfähigkeit
     "Ich habe in der Schule immer meine Abgabetermine eingehalten.",
+    //Durchsetzungsfähigkeit
     "Ich bin in der Lage andere von meiner Meinung zu überzeugen.",
     "Ich stehe zu meinen Überzeugungen, auch wenn sie von anderen kritisiert werden.",
     "Ich knicke unter Druck nicht ein.",
     "Ich laufe nicht vor Herausforderungen weg.",
     "Ich finde es leicht, Nein zu sagen, wenn ich etwas nicht tun möchte oder nicht für richtig halte.",
     //Aufgabenorientiert
-    "Frage 11:",
-    "Frage 12:",
-    "Frage 13:",
-    "Frage 14:",
-    "Frage 15:",
-    "Frage 16:",
-    "Frage 17:",
-    "Frage 18:",
-    "Frage 19:",
-    "Frage 20:",
-    "Frage 21:",
-    "Frage 22:",
-    "Frage 23:",
-    "Frage 24:",
-    "Frage 25:",
-    "Frage 26:",
-    "Frage 27:",
-    "Frage 28:",
-    "Frage 29:",
-    "Frage 30:",
-    "Frage 31:",
-    "Frage 32:",
-    "Frage 33:",
-    "Frage 34:",
-    "Frage 35:",
-    "Frage 36:",
-    "Frage 37:",
-    "Frage 38:",
-    "Frage 39:",
-    "Frage 40:"
+    "Ich priorisiere meine Aufgaben.",
+    "Ich beginne Aufgaben sofort und schiebe sie nicht vor mich hin.",
+    "Ich halte mich an meine Deadlines.",
+    "Ich hole mir Unterstützung oder zusätzliche Ressourcen, wenn ich sie brauche.",
+    "Ich bereite mich entsprechend auf meine Aufgaben vor.",
+    //Ruhig
+    "Ich lasse mich nicht leicht von anderen aus der Ruhe bringen.",
+    "Ich kann meine Emotionen gut kontrollieren, auch wenn ich verärgert bin.",
+    "Ich bleibe in stressigen Situationen ruhig und gelassen.",
+    "Ich gehe ruhig und systematisch an Probleme heran.",
+    "Ich kann ruhig bleiben, auch wenn mich jemand provoziert.",
+    //Direkt
+    "Ich sage klar und deutlich, was ich denke.",
+    "Ich vermeide es, um den heißen Brei herumzureden.",
+    "Ich kommuniziere offen über meine Bedürfnisse und Wünsche.",
+    "Ich konfrontiere andere direkt, wenn ich ein Problem mit ihrem Verhalten habe.",
+    "Ich finde es wichtig, offen und ehrlich zu kommunizieren, auch wenn es schwierig ist.",
+    //Freundlich
+    "Ich biete meine Hilfe an, wenn ich sehe, dass jemand sie benötigt.",
+    "Ich bedanke mich regelmäßig für kleine Gefälligkeiten.",
+    "Ich bin höflich und respektvoll, auch wenn ich anderer Meinung bin.",
+    "Ich biete oft meine Unterstützung an, ohne dass man mich darum bittet.",
+    "Ich bin höflich und zuvorkommend, selbst in stressigen Situationen.",
+    //Spontan
+    "Ich entscheide mich oft spontan für Aktivitäten oder Ausflüge.",
+    "Ich kann leicht auf unerwartete Ereignisse reagieren und mich anpassen.",
+    "Ich bin bereit, Pläne aufzugeben, wenn sich eine interessantere Gelegenheit bietet.",
+    "Ich nehme oft spontane Herausforderungen an, um meine Komfortzone zu erweitern.",
+    "Ich bin oft derjenige, der spontane Pläne für den Abend vorschlägt.",
+    //Impulsiv
+    "Ich handle oft, ohne vorher über die Konsequenzen nachzudenken.",
+    "Ich unterbreche andere häufig in Gesprächen, weil ich meine Gedanken sofort teilen möchte.",
+    "Ich habe Schwierigkeiten, meine Emotionen zu kontrollieren, wenn ich aufgeregt bin.",
+    "Ich gerate schnell in Streitigkeiten, weil ich impulsiv reagiere.",
+    "Ich fühle mich oft gezwungen, sofort zu handeln, wenn mir etwas in den Sinn kommt."
 ];
 let questionsIndex = 0;
 
@@ -49,6 +54,7 @@ const questionsText = document.getElementById('questions-place');
 const questionsForm = document.getElementById('question-form');
 const nextButton = document.getElementById('next');
 const backButton = document.getElementById('back');
+const endButton = document.getElementById('stop');
 
 function saveAnswer(index, answer){
     localStorage.setItem(`${index}_question`, answer);
@@ -83,6 +89,11 @@ function showNextQuestions() {
             questionsIndex++;
             loadQuestion(questionsIndex);
         }
+        else{
+            nextButton.classList.toggle('hidden');
+            endButton.classList.toggle('hidden');
+
+        }
     }
     
 }
@@ -91,6 +102,23 @@ function showLastQuestions(){
     if(questionsIndex > 0){
         questionsIndex--;
         loadQuestion(questionsIndex);
+        nextButton.classList.remove('hidden');
+        endButton.classList.add('hidden');
+    }
+}
+let categories = ["Pünktlich","Durchsetzungsfähig","Aufgabenorientiert"," Ruhig", "Direkt","Freundlich", "Spontan", "Impulsiv"];
+let c = 0;
+function end(){
+    for(let i = 0; i< questions.length; i += 5){
+        let x = 0;
+        for (let j = 0; j <5; j++){
+            let value = localStorage.getItem(`${i+j}_question`);
+            x += parseFloat(value)/5;
+        }
+        if(c < categories-length){
+            localStorage.setItem(`${categories[c]}`,Math.round(x));
+        }
+        c++;
     }
 }
 
@@ -105,3 +133,8 @@ backButton.addEventListener('click',(event) =>{
     event.preventDefault();
     showLastQuestions();
 } )
+
+endButton.addEventListener('click', (event) =>{
+    event.preventDefault();
+    end();
+})
